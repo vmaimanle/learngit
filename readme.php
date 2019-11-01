cbe8481ba8e36305cda694ddda8727ed793a6f51
Miraclo <597738979@qq.com>
2019-11-01 16:43:58 +0800

    understand how stage works

--- readme.php
+++ readme.php
@@ -3,6 +3,8 @@
 echo 'Git is a distributed version control system.';
 PHP_EOL;
 echo 'Git is free software distributed under the GPL.';
+PHP_EOL;
+echo 'Git has a mutable index called stage.';
 
 
 
